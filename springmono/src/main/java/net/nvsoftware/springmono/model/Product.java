d5ec38c0f33ddf114d70aad98ec8ae603b5dfec1
Susan Wang <susanwangqian02@gmail.com>
2024-08-18 11:34:31 +0800

    add product Model, Controller, Service, and Impl

---
+++ springmono/src/main/java/net/nvsoftware/springmono/model/Product.java
@@ -0,0 +1,30 @@
+package net.nvsoftware.springmono.model;
+
+public class Product {
+  private String productId;
+  private String title;
+  private String description;
+
+  public String getProductId() {
+    return productId;
+  }
+
+  public void setProductId(String productId) {
+    this.productId = productId;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+  public void setDescription(String description) {
+    this.description = description;
+  }
+}
